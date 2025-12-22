238a0d3331f4c0f943fe0d4cc5d7e73976a58796
ganjiejie <1067019870@qq.com>
2025-12-22 16:48:37 +0800

    feat：登录接口联调

--- src/contract/ethers-utils.js
+++ src/contract/ethers-utils.js
@@ -3,12 +3,47 @@ import { ethers } from 'ethers'
 import chainInfos from './chains'
 import detectEthereumProvider from '@metamask/detect-provider'
 import Web3 from 'web3'
+import { end, start } from "@/api/loading";
+import { login } from "@/api/user";
 import { walletStore } from '@/store/wallet'
 import { providerStore } from '@/store/provider'
 const wStore = walletStore()
 const pStore = providerStore()
 const contracts = import.meta.glob('./contracts/*.json')
 
+/**
+  * 参数是否被定义
+  */
+export async function isDefine(para) {
+  if (typeof para === 'undefined' || para === '' || para === null || para !== para) {
+    // para !== para checks for NaN
+    return false
+  }
+
+  if (typeof para === 'object' && Object.keys(para).length === 0 && para.constructor === Object) {
+    // checks for empty objects
+    return false
+  }
+
+  if (Array.isArray(para) && para.length === 0) {
+    // checks for empty arrays
+    return false
+  }
+
+  return true
+}
+/**
+ * 获取device
+ */
+export async function getDevice() {
+  let device = sessionStorage.getItem('device')
+  if (!await isDefine(device) || device.length === 0) {
+    device = uuid(32, 16)
+    sessionStorage.setItem('device', device)
+  }
+
+  return device
+}
 export async function asconnectWallet() {
   let provider, signer, account
   // 检测 MetaMask 注入的 provider
@@ -28,12 +63,31 @@ export async function asconnectWallet() {
     // 4. 等效签名（signMessage 对应 personal_sign）
     let signInfoName = 'wallet-tool-ethers'
     let baseURL = 'https://yimaibao.xyz'
-		const signTime = Date.now();
+    const signTime = Date.now();
     const message = signInfoName + ' wants you to sign in with your account:\n' + account + '\n\nSign in with account to the app.\n\nURI: ' + baseURL + '\nLogin time: ' + signTime
     const signature = await signer.signMessage(message);
     console.log("签名结果：", signature);
-
+    start()
     // 5.调用后端登陆接口
+    login({
+      address: account,
+      signTime: signTime,
+      signStr: message,
+      loginSign: signature,
+      device: await getDevice()
+    }).then(function (result) {
+      end()
+      const code = result.data?.code || 0
+      if (code === 1) {
+        const token = result.data.data.token
+        const salt = result.data.data.salt
+        console.log('token:', token)
+        console.log('salt:', salt)
+      } else {
+        alert(result.data.msg)
+      }
+    })
+
 
   } else {
     alert('请安装 MetaMask')
